80d8315de6aa2fc2ed72f6f6b3be247f2f857562
vasiltatarov <vasiltatarov3@gmail.com>
2021-05-15 12:28:44 +0300

    implement shoppingCartController and Order

---
+++ src/Data/OnlineShop.Data.Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineShop.Data.Models
+{
+    using OnlineShop.Data.Common.Models;
+
+    public class Order : BaseDeletableModel<int>
+    {
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool IsCheckout { get; set; }
+    }
+}
---
+++ src/Web/OnlineShop.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineShop.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ShoppingCartController : Controller
+    {
+        public ShoppingCartController()
+        {
+
+        }
+
+        public IActionResult Cart()
+        {
+            return this.View();
+        }
+    }
+}
